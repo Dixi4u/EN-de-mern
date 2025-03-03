66e960b1b677b4052ed8db00e8ab687a87131771
unknown <20200201@ricaldone.edu.sv>
2025-03-03 11:21:28 -0600

    inicio de EN

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+//Importo todo lo de la libreria express
+import express from "express"
+
+//Creo una constante que es igual a la libreria que acabo de importar, y la ejecuto
+const app = express();
+
+//Exporto esta constante para usar express en todos lados
+export default app;
