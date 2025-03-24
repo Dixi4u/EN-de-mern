abea61f56cf341f5624f21c3cab106820345513e
unknown <20200201@ricaldone.edu.sv>
2025-03-24 15:35:09 -0600

    AAAAAAAAAAAAAAA

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,25 @@
 //Importo todo lo de la libreria express
 import express from "express";
+import cookieParser from "cookie-parser";
 import productRoutes from "./src/routes/products.js"
 import clientsRoutes from "./src/routes/clients.js"
 import employeesRoutes from "./src/routes/employees.js"
 import branchesRoutes from "./src/routes/branches.js"
 import reviewsRoutes from "./src/routes/branches.js"
+import registerRoutes from "./src/routes/registerEmployees.js"
 
 //Creo una constante que es igual a la libreria que acabo de importar, y la ejecuto
+
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 
 app.use("/api/products", productRoutes);
 app.use("/api/clients", clientsRoutes);
 app.use("/api/employees", employeesRoutes);
 app.use("/api/branches", branchesRoutes);
 app.use("/api/reviews", reviewsRoutes);
+app.use("/api/registerEmployee", registerRoutes)
 
 
 //Exporto esta constante para usar express en todos lados
---
+++ backend/src/controllers/registerEmployeesController.js
@@ -0,0 +1,71 @@
+import employeeModel from "../models/Employees.js";
+import bcryptjs from "bcryptjs";
+import jsonwebtoken from "jsonwebtoken";
+import { config } from "../config.js";
+
+const registerEmployeesController = {};
+
+registerEmployeesController.register = async (req, res) => {
+  const {
+    name,
+    lastName,
+    birthday,
+    email,
+    address,
+    hireDate,
+    password,
+    telephone,
+    dui,
+    isssNumber,
+    isVerified,
+  } = req.body;
+
+  try {
+    //Verificamos si el empleado ya existe
+    const existingEmployee = await employeeModel.findOne({ email });
+    if (existingEmployee) {
+      return res.json({ message: "Empleado ya existe" });
+    }
+
+    //Encriptar la contraseña
+    const passwordHash = await bcryptjs.hash(password, 10);
+
+    //Guardamos el empleado nuevo
+    const newEmployee = new employeeModel({
+      name,
+      lastName,
+      birthday,
+      email,
+      address,
+      hireDate,
+      password: passwordHash,
+      telephone,
+      dui,
+      isssNumber,
+      isVerified,
+    });
+
+    await newEmployee.save()
+
+    //--> Token <--
+    jsonwebtoken.sign(
+        //1-Que voy a guardar
+        {id: newEmployee._id},
+        //2-Secreto
+        config.JWT.secret,
+        //3-Cuando expira
+        {expiresIn: config.JWT.expiresIn},
+        //4-Funcion flecha
+        (error, token) =>{
+            if(error) console.log(error)
+                res.cookie("authToken", token)
+        }
+    )
+
+
+  } catch (error) {
+
+  }
+};
+
+export default registerEmployeesController
