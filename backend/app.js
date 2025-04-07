8bf48b03242ba8e258bdbd37199444a7f8e22e28
unknown <20200201@ricaldone.edu.sv>
2025-04-07 15:44:08 -0600

    registro con codigo

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,15 @@
 //Importo todo lo de la libreria express
 import express from "express";
 import cookieParser from "cookie-parser";
-import productRoutes from "./src/routes/products.js"
-import clientsRoutes from "./src/routes/clients.js"
-import employeesRoutes from "./src/routes/employees.js"
-import branchesRoutes from "./src/routes/branches.js"
-import reviewsRoutes from "./src/routes/branches.js"
-import registerRoutes from "./src/routes/registerEmployees.js"
+import productRoutes from "./src/routes/products.js";
+import clientsRoutes from "./src/routes/clients.js";
+import employeesRoutes from "./src/routes/employees.js";
+import branchesRoutes from "./src/routes/branches.js";
+import reviewsRoutes from "./src/routes/branches.js";
+import registerRoutes from "./src/routes/registerEmployees.js";
 import loginRoute from "./src/routes/login.js";
-import logoutRoute from "./src/routes/logout.js"
+import logoutRoute from "./src/routes/logout.js";
+import registerClients from "./src/routes/registerClients.js";
 
 //Creo una constante que es igual a la libreria que acabo de importar, y la ejecuto
 
@@ -24,6 +25,8 @@ app.use("/api/reviews", reviewsRoutes);
 app.use("/api/registerEmployee", registerRoutes)
 app.use("/api/login", loginRoute)
 app.use("/api/logout", logoutRoute)
+
+app.use("/api/registerClients", registerClients)
 
 
 //Exporto esta constante para usar express en todos lados
